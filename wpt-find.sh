d39ac5152ee1fe924651486835fa79f046988926
Brandon McConnell <brandon@dreamthinkbuild.com>
2023-05-29 00:14:28 -0700

    Add support for title scraping from HTML files

--- wpt-find.sh
+++ wpt-find.sh
@@ -5,12 +5,14 @@ function wpt-find() {
   local directory="/wpt" # Replace this with your local `wpt` checkout path
   local md=0
   local list=0
+  local title=0
   local clipboard=0
-  local green='\033[0;32m'  # Results text color
-  local blue='\033[0;34m'  # Results links color (when rendering markdown)
-  local nc='\033[0m'  # Default text color
+  local color_none='\033[0m'      # Default text color
+  local color_base='\033[0;32m'   # Result text color
+  local color_link='\033[0;34m'   # Result link color (used in markdown)
+  local color_line='\033[38;5;8m'   # Neutral gray for separators
 
-  local usage_string="Usage: $(basename $0) [-m|--markdown] [-l|--list] [-c|--copy] \"[searchterm]\""
+  local usage_string="Usage: $(basename $0) [-m|--markdown] [-l|--list] [-t|--title] [-c|--copy] \"[searchterm]\""
 
   # Handling long-form arguments
   for arg in "$@"
@@ -19,18 +21,22 @@ function wpt-find() {
     case "$arg" in
       "--markdown") set -- "$@" "-m" ;;
       "--list") set -- "$@" "-l" ;;
+      "--title") set -- "$@" "-t" ;;
       "--copy") set -- "$@" "-c" ;;
       *) set -- "$@" "$arg"
     esac
   done
 
-  while getopts 'mlc' OPTION; do
+  while getopts 'mltc' OPTION; do
     case "$OPTION" in 
       m) 
         md=1
         ;;
       l)
         list=1
+        ;;
+      t)
+        title=1
         ;;
       c)
         clipboard=1
@@ -53,7 +59,7 @@ function wpt-find() {
   local results=($(grep -ElR "$1" | grep ".html$" | grep -v "[-]ref.html$" | sed -e "s,^\.,https://wpt.fyi,"))
   local filtered_results=()
   local link_prefix="https://wpt.fyi"
-  local output_string=""
+  local copy_string=""
   local print_string=""
   for item in "${results[@]}"; do
     if [[ $item == *"$link_prefix"* ]]; then
@@ -67,41 +73,49 @@ function wpt-find() {
   fi
 
   local index=0
-  local result_count=${#filtered_results[*]}
-  if [ $result_count -gt 0 ]; then
-    printf '%*s\n' "$columns" '' | tr ' ' -
+  local count=${#filtered_results[*]}
+  if [ $count -gt 0 ]; then
     for item in "${filtered_results[@]}"; do
-      local print_prefix=""
-      [[ $list -eq 1 ]] && print_prefix="- "
+      local list_prefix=""
+      [[ $list -eq 1 ]] && list_prefix="- "
       local print_link="${item}"
       local abbr_link="${print_link#$link_prefix}"
       if [[ $md -eq 1 ]]; then
-        output_string+="${print_prefix}[${abbr_link}](${print_link})"
-        print_link="${print_prefix}${nc}[${green}${abbr_link}${nc}](${blue}${print_link}${nc})"
+        if [[ $title -eq 1 ]]; then
+          local html_path="${directory}${abbr_link}"
+          local title_text=$(sed -n -e 's|^.*<title>\(.*\)</title>.*$|\1|p' "$html_path")
+          title_text=$(echo "$title_text" | tr -d '\n' | awk '{$1=$1};1' | tr -s " ")
+          [[ -z "$title_text" ]] && title_text="$abbr_link"
+          copy_string+="${list_prefix}[${title_text}](${print_link})"
+          print_link="${list_prefix}${color_none}[${color_base}${title_text}${color_none}](${color_link}${print_link}${color_none})"
+        else
+          copy_string+="${list_prefix}[${abbr_link}](${print_link})"
+          print_link="${list_prefix}${color_none}[${color_base}${abbr_link}${color_none}](${color_link}${print_link}${color_none})"
+        fi
       else
-        output_string+="${print_prefix}${print_link}"
-        print_link="${print_prefix}${green}${print_link}${nc}"
+        copy_string+="${list_prefix}${print_link}"
+        print_link="${list_prefix}${color_base}${print_link}${color_none}"
       fi
       print_string+="${print_link}"
-      if [[ "$index" -ne $((result_count - 1)) ]]; then
-        output_string+="\n"
+      if [[ "$index" -ne $((count - 1)) ]]; then
+        copy_string+="\n"
         print_string+="\n"
       fi
       index=$((index + 1))
     done
-    echo -e "${print_string}"
-    printf '%*s\n' "$columns" '' | tr ' ' -
-    echo "\nFound ${result_count} tests related to your search term \"$1\""
+    local line_separator=$(printf "${color_line}%*s${color_none}\n" "$columns" '' | tr ' ' ─)
+    echo -e "$line_separator\n$print_string\n$line_separator\n"
+    echo "Found ${count} tests related to your search term \"$1\""
     if [[ $clipboard -eq 1 ]]; then 
       local clipboard_msg="\nResults have been copied to your clipboard."
       if command -v pbcopy > /dev/null; then
-        echo -n "$output_string" | pbcopy
+        echo -n "$copy_string" | pbcopy
         echo "$clipboard_msg"
       elif command -v xclip > /dev/null; then
-        echo -n "$output_string" | xclip -selection clipboard
+        echo -n "$copy_string" | xclip -selection clipboard
         echo "$clipboard_msg"
       elif command -v clip > /dev/null; then
-        echo -n "$output_string" | clip
+        echo -n "$copy_string" | clip
         echo "$clipboard_msg"
       else
         echo "Could not copy to the clipboard. Please install 'pbcopy' (macOS), 'xclip' (Linux), or 'clip' (Windows) to enable this feature."
